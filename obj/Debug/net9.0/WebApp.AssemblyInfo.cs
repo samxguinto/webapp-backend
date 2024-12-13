08e3cc591c0fca4c459ef924c07e656635fc3760
spammyg413 <spamguinto@gmail.com>
2024-12-12 19:01:11 -0800

    finished deployment please dont break

--- obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84edfaa9019ea308951defe06e43f9ed608b13cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9de810fd0e957027b96efa0dec989a4ca80fba")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
